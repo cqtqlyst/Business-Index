1dfd6f0f94050e59268a53b0f80bb93cf9a11021
cqtqlyst <aayush.gaywala@gmail.com>
2024-02-20 15:28:51 -0800

    stuck on missing or insufficient permissions error

--- next-app/src/app/search/page.js
+++ next-app/src/app/search/page.js
@@ -1,8 +1,25 @@
 "use client"
 
+import React, { useState, useEffect } from 'react';
+import {collection, getDocs} from "firebase/firestore";
 import Nav from "@/components/nav";
+import Business from "@/components/business";
+import db from '../firebase';
 
 export default function Home() {
+
+    const data = getData();
+    
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredData, setFilteredData] = useState(data);
+
+    const handleSubmit = (event) => {
+        setSearchTerm(event.target.value);
+        const filtered = data.filter((item) =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredData(filtered);
+    };
 
     return (
         <div>
@@ -10,10 +27,29 @@ export default function Home() {
             <div className="flex justify-center py-10">
                 <input 
                     className="bg-black font-mono text-5xl placeholder-gray-300 text-white w-1/2 text-center" 
-                    placeholder="Enter key words to find your business.">
+                    placeholder="Enter key words to find your business."
+                    onSubmit={handleSubmit}>
                 </input>
+                {/* {filteredData.map((item) => (
+                    <Business key={item.id} {...item} />
+                ))} */}
             </div>
         </div>
     );
+
+}
+   
+async function getData() {
+
+    const data = await collection(db, 'businesses');
+    const docs = getDocs(data);
+    
+    console.log(docs);
+
+    // const dataDocs = docs.map((doc) => doc.data);
+
+    // console.log(dataDocs[0].name);
+
+    return [];
 
 }
