5e7bea23adff09044ded5fc99b8baeed5d0b1620
cqtqlyst <aayush.gaywala@gmail.com>
2024-02-20 00:05:04 -0800

    got enter partially done

--- next-app/src/app/page.js
+++ next-app/src/app/page.js
@@ -6,7 +6,7 @@ import Nav from "@/components/nav";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
       <Nav/>
 
     </div>
--- next-app/src/app/search/page.js
+++ next-app/src/app/search/page.js
@@ -7,8 +7,11 @@ export default function Home() {
     return (
         <div>
             <Nav/>
-            <div>
-                
+            <div className="flex justify-center py-10">
+                <input 
+                    className="bg-black font-mono text-5xl placeholder-gray-300 text-white w-1/2 text-center" 
+                    placeholder="Enter key words to find your business.">
+                </input>
             </div>
         </div>
     );
