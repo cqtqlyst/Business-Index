36ba105f9c63174e7d9cc65b3c07a98dde02a5f6
cqtqlyst <aayush.gaywala@gmail.com>
2024-02-19 23:13:41 -0800

    added firebase

---
+++ next-app/src/app/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBOopwWmx-Y28dvyoU070AiyJk1PV9AHOM",
+  authDomain: "hhs-cap.firebaseapp.com",
+  projectId: "hhs-cap",
+  storageBucket: "hhs-cap.appspot.com",
+  messagingSenderId: "637040098581",
+  appId: "1:637040098581:web:8b2deea8503ce3fe193d1c",
+  measurementId: "G-T8DWSY5DZP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
