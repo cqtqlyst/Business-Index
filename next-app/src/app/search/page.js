95eeb9fe2b27b59b7cc00a770d472a8ca72ccb41
cqtqlyst <aayush.gaywala@gmail.com>
2024-02-21 21:10:06 -0800

    search done, not touching it

--- next-app/src/app/search/page.js
+++ next-app/src/app/search/page.js
@@ -1,50 +1,53 @@
 "use client"
 
 import React, { useState, useEffect } from 'react';
-import {collection, getDocs} from "firebase/firestore";
+import {collection, getDocs, query} from "firebase/firestore";
 import Nav from "@/components/nav";
 import Business from "@/components/business";
 import db from '../firebase';
 
 export default function Home() {
 
-    let fetched = false;
-
-    const [allData, setData] = useState([]);
+    const [fetched, setFetched] = useState(false);
+    const [allData, setData] = useState(null);
     const [searchTerm, setSearchTerm] = useState('');
     const [filteredData, setFilteredData] = useState([]);
     
     function getData() {
-        const data = collection(db, "businesses");
+        const data = query(collection(db, "businesses"));
         let results = [];
 
-        if (!fetched) {
+        if (allData == null && fetched == false) {
+            setFetched(true);
             getDocs(data).then((dataSnapshot) => {
                 dataSnapshot.forEach((doc) => {
                     results.push(doc.data());
                 })
                 setData(results);
-                fetched = true;
             });
-            console.log(allData);
         }
+        console.log(allData);
     }
 
     useEffect(() => {getData()});
 
     const handleSubmit = (event) => {
+
+        // console.log(event);
         setSearchTerm(event.target.value);
 
-        // console.log("search term" + searchTerm);
+        let filtered = allData.filter((item) => {
 
-        const filtered = allData.filter((item) => {
-            console.log("item.name" + item.name);
-            console.log("search term" + searchTerm);
-            return item.name.includes(searchTerm);
+            // console.log("item.name " + item.Name);
+            // console.log("search term " + searchTerm);
+
+            return item.Name.toLowerCase().includes(searchTerm.toLowerCase());
         });
+
         setFilteredData(filtered);
-        console.log("filtered");
-        console.log(filteredData);
+
+        // console.log("filtered");
+        // console.log(filteredData);
     };
 
     return (
@@ -60,9 +63,12 @@ export default function Home() {
                         }
                     }}>
                 </input>
-                {/* {filteredData.map((item) => (
-                    <Business key={item.id} {...item} />
-                ))} */}
+            </div>
+            <div className="justify-center container mx-auto flex flex-wrap py-20">
+                {filteredData.map((item) => (
+                    <Business
+                    name={item.Name} website={item.Website} email={item.Email} address={item.Address}/>
+                ))}
             </div>
         </div>
     );
--- next-app/src/components/business.js
+++ next-app/src/components/business.js
@@ -1,5 +1,13 @@
 
 export default function Business(props) {
 
-
+    return (
+        <div className="mb-8 ml-10 mr-10">
+            <a href={"https://" + props.website} target="_blank" class="block max-w-sm p-6 bg-black border border-gray-200 rounded-lg shadow ">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">{props.name}</h5>
+                <p class="font-normal text-white dark:text-gray-400">The address is {props.address}.</p>
+                <p class="font-normal text-white dark:text-gray-400">The email for contact is {props.email}.</p>
+            </a>
+        </div>
+    );
 }
