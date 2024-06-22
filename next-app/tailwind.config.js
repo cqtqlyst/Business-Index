6b202513cc29b9baa8d9ec4379b7cb6eb273ddc8
cqtqlyst <aayush.gaywala@gmail.com>
2024-06-21 19:56:25 -0700

    imported and added milanello

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'bright-purple': '#802BB1',
+        'dark-purple': '#2D283E',
+        'meloncholy': '#564F6F',
+        'gray': '#4C495D',
+        'ivory': '#D1D7E0',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
