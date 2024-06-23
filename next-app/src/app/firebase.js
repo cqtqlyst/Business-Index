d66dd8c2afed771f10d2c683cd751b1e48823747
cqtqlyst <aayush.gaywala@gmail.com>
2024-06-23 15:34:08 -0700

    added login component + footer class

--- next-app/src/app/firebase.js
+++ next-app/src/app/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,4 +26,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+export const auth = getAuth(app);
 export default db;
