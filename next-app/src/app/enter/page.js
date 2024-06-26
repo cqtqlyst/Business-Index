f02e054df0e1e92a131d4c8b9e3c8abfd3c48ed3
Dani Iacob <dan.leo.iacob@gmail.com>
2024-06-26 02:16:41 -0700

    restyled the enter page. syntactically checks for int-only vals. got rid of arrows

--- next-app/src/app/enter/page.js
+++ next-app/src/app/enter/page.js
@@ -70,72 +70,73 @@ export default function Home() {
             <Nav/>
             <div>
                 <div className="container mx-auto py-10">
-                    <form id="myForm" className="max-w-sm mx-auto border border-gray-300 rounded-lg shadow-lg p-8">
+                    <form id="myForm" className="max-w-xl mx-auto border border-gray-300 rounded-lg shadow-lg p-8">
                         <div class="mb-5">
-                            <label for="Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Business Name</label>
+                            <label for="Name" class="block mb-2 text-sm font-medium text-white">Business Name</label>
                             <input 
                                 type="text" 
                                 // placeholder="Business Name" required 
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
+                                className="shadow-sm bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-4 focus:outline-none focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5 placeholder-gray-400" 
                             />
                         </div>
                         <div class="mb-5">
-                            <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+                            <label for="address" class="block mb-2 text-sm font-medium text-white">Address</label>
                             <input 
                                 type="address" 
                                 placeholder="1234 First Ave. New York, NY" required 
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
+                                className="shadow-sm bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-4 focus:outline-none focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5 placeholder-gray-400" 
                             />
                         </div>
                         <div class="mb-5">
-                            <label for="Service" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service</label>
+                            <label for="Service" class="block mb-2 text-sm font-medium text-white">Service</label>
                             <input 
                                 type="text" 
                                 // placeholder="What Services Does Your Business Offer" required 
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
+                                className="shadow-sm bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-4 focus:outline-none focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5 placeholder-gray-400" 
                             />
                         </div>
                         <div class="mb-5">
-                            <label for="Legal Structure" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Legal Structure</label>
+                            <label for="Legal Structure" class="block mb-2 text-sm font-medium text-white">Legal Structure</label>
                             <input 
                                 type="text" 
                                 // placeholder="Enter what legal structure the business has" required 
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
+                                className="shadow-sm bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-4 focus:outline-none focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5 placeholder-gray-400" 
                             />
                         </div>
                         <div class="mb-5">
-                            <label for="NAICS Code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NAICS Code</label>
+                            <label for="NAICS Code" class="block mb-2 text-sm font-medium text-white">NAICS Code</label>
                             <input 
-                                type="text" 
+                                type="number" 
                                 // placeholder="Enter what NAICS code the business falls under" required 
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
+                                className="remove-arrow shadow-sm bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-4 focus:outline-none focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5 placeholder-gray-400" 
                             />
                         </div>
                         <div class="mb-5">
-                            <label for="EmployeeCount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number Of Employees</label>
-                            <input 
-                                type="text" 
+                            <label for="EmployeeCount" class="block mb-2 text-sm font-medium text-white">Number Of Employees</label>
+                            <input type="number" 
                                 // placeholder="Enter the number of employees the business has"
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
+                                className="remove-arrow shadow-sm bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-4 focus:outline-none focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5 placeholder-gray-400" 
                             />
                         </div>
                         <div class="mb-5">
-                            <label for="WebsiteLink" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Website Link</label>
+                            <label for="WebsiteLink" class="block mb-2 text-sm font-medium text-white">Website Link</label>
                             <input 
                                 type="url" 
                                 placeholder="https://WebsiteLink.com" required 
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
+                                className="shadow-sm bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-4 focus:outline-none focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5 placeholder-gray-400" 
                             />
                         </div>
                         <div class="mb-5">
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Address</label>
+                            <label for="email" class="block mb-2 text-sm font-medium text-white">Email Address</label>
                             <input 
                                 type="email" 
                                 placeholder="name@company.com" required 
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
+                                className="shadow-sm bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-4 focus:outline-none focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5 placeholder-gray-400" 
                             />
                         </div>
-                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleSubmit} >Submit</button>
+                        <div className="flex justify-center">
+                            <button type="submit" className="text-white bg-purple-500 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center" onClick={handleSubmit} >Submit</button>
+                        </div>
                     </form>
                 </div>
             </div>
