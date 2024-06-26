0bf11594e39e3b2afab7be7c93db09937492435b
Dani Iacob <dan.leo.iacob@gmail.com>
2024-06-25 19:50:52 -0700

    two terminal errors fixed

--- next-app/src/app/login/page.js
+++ next-app/src/app/login/page.js
@@ -45,7 +45,7 @@ export default function Home () {
                         Flowbite    
                     </a> */}
                     <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-purply-gray dark:border-gray-700">
-                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Sign in to your account
                             </h1>
@@ -67,7 +67,7 @@ export default function Home () {
                                             <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
                                         </div>
                                         <div class="ml-3 text-sm">
-                                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                            <label For="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                                         </div>
                                     </div>
                                     <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
