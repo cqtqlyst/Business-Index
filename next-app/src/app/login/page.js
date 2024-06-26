ca4997ba815a346b45fa27ac92cd193115abc763
cqtqlyst <aayush.gaywala@gmail.com>
2024-06-26 15:14:32 -0700

    restyling some login stuff

--- next-app/src/app/login/page.js
+++ next-app/src/app/login/page.js
@@ -5,6 +5,7 @@ import { useRef } from "react"
 import { auth } from '@/app/firebase.js';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import Nav from "@/components/nav";
+import Footer from "@/components/footer"
 
 export default function Home () {
  
@@ -38,13 +39,13 @@ export default function Home () {
     return (
         <div>
             <Nav/>
-            <section>
-                <div class="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
+            <section className="py-28 mb-14">
+                <div class="flex flex-col items-center justify-center px-6 mx-auto lg:py-0">
                     {/* <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                         <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
                         Flowbite    
                     </a> */}
-                    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-purply-gray dark:border-gray-700">
+                    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-regular-purple dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Sign in to your account
@@ -90,6 +91,7 @@ export default function Home () {
                     </div>
                 </div>
             </section>
+            <Footer/>
         </div>
     )
 }
