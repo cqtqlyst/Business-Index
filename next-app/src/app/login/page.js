275725167fb8800b54d585cb24677a2b074e85dd
Dani Iacob <dan.leo.iacob@gmail.com>
2024-06-26 16:58:41 -0700

    fixing some terminal errors

--- next-app/src/app/login/page.js
+++ next-app/src/app/login/page.js
@@ -28,7 +28,6 @@ export default function Home () {
                 //router to next page
                 window.location.href =
                     '/';
- 
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -40,50 +39,46 @@ export default function Home () {
         <div>
             <Nav/>
             <section className="py-28 mb-14">
-                <div class="flex flex-col items-center justify-center px-6 mx-auto lg:py-0">
-                    {/* <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                        <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-                        Flowbite    
-                    </a> */}
-                    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-regular-purple dark:border-gray-700">
+                <div className="flex flex-col items-center justify-center px-6 mx-auto lg:py-0">
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-regular-purple dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Sign in to your account
                             </h1>
                             <form 
-                                class="space-y-4 md:space-y-6" 
+                                className="space-y-4 md:space-y-6" 
                                 action="#"
                                 onSubmit={login}>
                                 <div>
-                                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                     <div class="relative">
-                                            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                                            <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
                                                     <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
                                                     <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
                                                 </svg>
                                             </div>
                                             <input type="text" id="email-address-icon" ref={logemailRef} 
-                                            class="border text-gray-900 rounded-lg block w-full ps-10 p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:none focus:border-blue-500" placeholder="name@company.com" required=""/>
+                                            className="border text-gray-900 rounded-lg block w-full ps-10 p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:none focus:border-blue-500" placeholder="name@company.com" required=""/>
                                         </div>
                                     </div>
                                 <div>
-                                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                                     <input ref={logpasswordRef} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                                 </div>
-                                <div class="flex items-center justify-between">
-                                    <div class="flex items-start">
-                                        <div class="flex items-center h-5">
+                                <div className="flex items-center justify-between">
+                                    <div className="flex items-start">
+                                        <div className="flex items-center h-5">
                                             <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
                                         </div>
-                                        <div class="ml-3 text-sm">
+                                        <div className="ml-3 text-sm">
                                             <label For="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                                         </div>
                                     </div>
-                                    <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                                 </div>
-                                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                     Don’t have an account yet? <a href="/signup" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                                 </p>
                             </form>
