7d33376688290578501e063d6425bedc776c8b94
Mihir-A <mihiranan@gmail.com>
2024-06-29 18:08:06 -0400

    fix backup

--- next-app/src/app/backup.js
+++ next-app/src/app/backup.js
@@ -12,11 +12,6 @@ const backupData = async () => {
 
         // Get all documents from the backup collection
         const backupCollection = collection(db, "backup");
-        const backupSnapshot = await getDocs(backupCollection);
-        const backupData = backupSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-
-        // Create a map for the backup data for quick lookup
-        const backupMap = new Map(backupData.map(doc => [doc.id, doc]));
 
         // Sync each document in the businesses collection with the backup collection
         for (const business of businessesData) {
@@ -24,12 +19,12 @@ const backupData = async () => {
             const backupDoc = await getDoc(backupDocRef);
 
             if (!backupDoc.exists() || JSON.stringify(backupDoc.data()) !== JSON.stringify(business)) {
-                // If the document doesn't exist in the backup collection or it has different data, add/update it
+                // If the document doesn't exist in the backup collection or it has different data, 
+                //add/update it
                 await setDoc(backupDocRef, business);
             }
         }
 
-        console.log("Backup complete.");
     } catch (error) {
         console.error("Error backing up data: ", error);
     }
